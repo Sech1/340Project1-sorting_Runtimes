b08bd8f70b7a8328c0006e23961facc377985533
Jared M Schooley <jaschoo@siue.edu>
2019-01-30 15:22:58 -0600

    added a list of files to run on homeserver

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ from threading import Thread
 from execSortingAndTiming import *
 from util import *
 
-threadAlgo = Thread(target=ExecSortingAndTiming.exec_insertion_sort_perm, args=("30K",))
-spinnerThread = Thread(target=util.spinning_cursor)
+allPermList = ["15K", "30K", "45K", "60K", "75K", "90K", "105K", "120K", "135K", "150K"]
+smallPermList = ["15K", "30K", "45K", "60K"]
 
-threadAlgo.start()
-spinnerThread.start()
-threadAlgo.join()
-util.go = False
+def main():
+    runAllPerm()
+
+
+def runAllPerm():
+    for x in range(len(smallPermList)):
+        threadAlgo = Thread(target=ExecSortingAndTiming.exec_insertion_sort_perm, args=(smallPermList[x],))
+        spinnerThread = Thread(target=util.spinning_cursor)
+
+        threadAlgo.start()
+        spinnerThread.start()
+        threadAlgo.join()
+        util.go = False
+
+
+if __name__ == '__main__':
+    main()
