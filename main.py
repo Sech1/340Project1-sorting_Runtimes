70927ea1935a6b84737ea16d4125a6088779ba2a
Jared M Schooley <jaschoo@siue.edu>
2019-01-30 15:51:10 -0600

    text formatting

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ from util import *
 
 allPermList = ["15K", "30K", "45K", "60K", "75K", "90K", "105K", "120K", "135K", "150K"]
 smallPermList = ["15K", "30K", "45K", "60K"]
+testPermList = ["15K"]
+
 
 def main():
     runAllPerm()
 
 
 def runAllPerm():
-    for x in range(len(smallPermList)):
-        threadAlgo = Thread(target=ExecSortingAndTiming.exec_insertion_sort_perm, args=(smallPermList[x],))
-        spinnerThread = Thread(target=util.spinning_cursor)
+    spinnerThread = Thread(target=util.spinning_cursor)
+    spinnerThread.start()
+    for x in range(len(testPermList)):
+        threadAlgo = Thread(target=ExecSortingAndTiming.exec_insertion_sort_perm, args=(testPermList[x],))
 
         threadAlgo.start()
-        spinnerThread.start()
         threadAlgo.join()
-        util.go = False
+
+    util.go = False
+    spinnerThread.join()
 
 
 if __name__ == '__main__':
